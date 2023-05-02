3aa1c60388f7cb13e892bc4ccef266bff400562f
Valentina-Borges <108351920+Valentina-Borges@users.noreply.github.com>
2023-05-02 16:15:09 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+song ="";
+
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw() {
+    Image(video, 0, 0, 600, 500);
+}
